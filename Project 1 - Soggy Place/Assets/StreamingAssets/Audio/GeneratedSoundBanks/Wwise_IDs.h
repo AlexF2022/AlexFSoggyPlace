0a66202ff2516caf24228f56191a814da3212331
AlexF2022 <97470597+AlexF2022@users.noreply.github.com>
2022-01-29 13:31:10 -0800

    Added Unity Project

---
+++ Project 1 - Soggy Place/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,45 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_ALARM = 603937113U;
+        static const AkUniqueID PLAY_BARN = 1577305889U;
+        static const AkUniqueID PLAY_FIRE = 3015324718U;
+        static const AkUniqueID PLAY_FIREFIREPLACE01 = 4069430882U;
+        static const AkUniqueID PLAY_HOUSE1 = 4214932215U;
+        static const AkUniqueID PLAY_NATURE_AMBIENCE = 3128628778U;
+        static const AkUniqueID PLAY_SHREK = 2470770091U;
+        static const AkUniqueID PLAY_WINDCHIMES = 2917879851U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
